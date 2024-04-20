def18b4525d9c927e36e18e96e735410e5ec78dd
sndpoffcl <sandeep2official@gmail.com>
2024-04-20 22:47:25 +0530

    Added UUIDs

--- src/main/java/dev/sandeep/EcomProductServiceDec23/entity/BaseModel.java
+++ src/main/java/dev/sandeep/EcomProductServiceDec23/entity/BaseModel.java
@@ -4,18 +4,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
+import java.util.UUID;
 
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private UUID id;
     @CreationTimestamp
-    private Instant createdAt;  // number of milliseconds since 1 Jan, 1970 UTC
+    private Instant createdAt;  // number of seconds/nanoseconds since 1 Jan, 1970 UTC
     @UpdateTimestamp
-    private Instant updatedAt; // number of milliseconds since 1 Jan, 1970 UTC
+    private Instant updatedAt; // number of seconds/nanoseconds since 1 Jan, 1970 UTC
 }

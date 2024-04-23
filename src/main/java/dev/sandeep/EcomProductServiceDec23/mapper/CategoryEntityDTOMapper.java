c7522d6a989d2c0f8afa6c890b8d9475a108990d
sndpoffcl <sandeep2official@gmail.com>
2024-04-23 23:10:02 +0530

    Category mapper added

---
+++ src/main/java/dev/sandeep/EcomProductServiceDec23/mapper/CategoryEntityDTOMapper.java
@@ -0,0 +1,10 @@
+package dev.sandeep.EcomProductServiceDec23.mapper;
+
+import dev.sandeep.EcomProductServiceDec23.dto.CategoryResponseDTO;
+import dev.sandeep.EcomProductServiceDec23.entity.Category;
+
+public class CategoryEntityDTOMapper {
+    public static CategoryResponseDTO convertCategoryToCategoryResponseDTO(Category category){
+        //call product entity to product response dto mapper
+    }
+}
